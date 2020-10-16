01b819e023fe26bcb5db9520cd7fcdf9991ca2f6
Harikiranreddykottala <khkreddy96@gmail.com>
2020-10-16 10:49:19 +0530

    py file added

---
+++ air.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Oct 16 10:09:49 2020
+
+@author: khkre
+"""
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
